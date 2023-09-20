17418b2a4e91135e204f8ac611592daf4d7aa7d3
trippyzxc <trippyzxc@gmail.com>
2023-09-20 20:54:07 +0800

    polished

--- crud/config/settings.py
+++ crud/config/settings.py
@@ -29,6 +29,7 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_METHODS = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS']
 
 
 # Application definition
